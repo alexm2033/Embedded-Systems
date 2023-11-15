e18b4e334c85e0ec54856fb15a88ba370dc2d3ce
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2023-11-15 21:53:56 +0000

    challenge 382

--- Tasks/Task-382-MemoryPool/main.cpp
+++ Tasks/Task-382-MemoryPool/main.cpp
@@ -12,9 +12,16 @@ void thread1();
 void thread2();
 void switchISR();
 
+InterruptIn btnA(BTN1_PIN);
+InterruptIn btnB(BTN2_PIN);
+
+
+
+const uint32_t TIMEOUT_mS = 30000;
 
 //Threads
 Thread t1;
+Thread t2;
 
 //Class type
 class message_t {
@@ -39,36 +46,8 @@ Queue<message_t, 16> queue;
 
 // Call this on precise intervals
 void switchISR() {
-    
-    //Read sample - make a copy
-    float sample = 0.01f*(float)(rand() % 100);
-
-    //Grab switch state
-    uint32_t switch1State = buttonA;
-    uint32_t switch2State = buttonB;
-    
-    //Allocate a block from the memory pool (non blocking)
-    message_t* message = mpool.try_alloc();
-    if (message == NULL) {
-        //Out of memory
-        redLED = 1;
-        return;   
-    }
-    
-    //Fill in the data
-    message->fValue = sample;
-    message->sw1State = switch1State;
-    message->sw2State = switch2State;
-    
-    //Write to queue
-    bool ok = queue.try_put(message);    //Note we are sending the "pointer"
-    
-    //Check if succesful
-    if (!ok) {
-        redLED = 1; 
-        mpool.free(message);
-        return;
-    }
+    t2.flags_set(1);
+    Watchdog::get_instance().kick();
 }
 
 //Normal priority thread (consumer)
@@ -99,22 +78,82 @@ void thread1()
     } //end while
 }
 
+void thread2() {
+    while(true) {
+
+        ThisThread::flags_wait_all(1);
+        ThisThread::sleep_for(100ms);
+        ThisThread::flags_clear(1);
+            //Read sample - make a copy
+        float sample = 0.01f*(float)(rand() % 100);
+
+        //Grab switch state
+        uint32_t switch1State = buttonA;
+        uint32_t switch2State = buttonB;
+
+        
+        //Allocate a block from the memory pool (non blocking)
+        message_t* message = mpool.try_alloc();
+        if (message == NULL) {
+            //Out of memory
+            redLED = 1;
+            return;   
+        }
+        
+        //Fill in the data
+        message->fValue = sample;
+        message->sw1State = switch1State;
+        message->sw2State = switch2State;
+        
+        //Write to queue
+        bool ok = queue.try_put(message);    //Note we are sending the "pointer"
+        
+        //Check if succesful
+        if (!ok) {
+            redLED = 1; 
+            mpool.free(message);
+            return;
+        }
+    }
+}
+
 
 // Main thread
 int main() {
+    Watchdog &watchdog = Watchdog::get_instance();
+    watchdog.start(TIMEOUT_mS);
+
     redLED    = 0;
     yellowLED = 0;
     greenLED  = 0;
-           
+
+    ThisThread::sleep_for(100ms);
+
+    redLED    = 1;
+    yellowLED = 1;
+    greenLED  = 1;
+
+    ThisThread::sleep_for(500ms);
+
+    redLED    = 0;
+    yellowLED = 0;
+    greenLED  = 0;
+
+    watchdog.start(TIMEOUT_mS);
+
+    btnA.fall(&switchISR);
+    btnB.fall(&switchISR);
+          
     //Start message
     printf("Welcome\n");           
    
-    //Hook up interrupts   
-    Ticker timer; 
-    timer.attach(&switchISR, 100ms);
+    // //Hook up interrupts   
+    // Ticker timer; 
+    // timer.attach(&switchISR, 100ms);
                
     //Threads
     t1.start(thread1);
+    t2.start(thread2);
     
     printf("Main Thread\n");
     while (true) {
@@ -122,3 +161,4 @@ int main() {
         puts("Main Thread Alive");
     }
 }
+
