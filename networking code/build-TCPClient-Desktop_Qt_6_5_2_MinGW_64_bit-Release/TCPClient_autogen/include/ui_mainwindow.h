c1df1c0f140987878f793706286772d1d4777b34
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2023-11-17 16:03:26 +0000

    task 394

---
+++ networking code/build-TCPClient-Desktop_Qt_6_5_2_MinGW_64_bit-Release/TCPClient_autogen/include/ui_mainwindow.h
@@ -0,0 +1,184 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.5.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_4;
+    QPushButton *sendButton;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label;
+    QLineEdit *ip1;
+    QLineEdit *ip2;
+    QLineEdit *ip3;
+    QLineEdit *ip4;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label_2;
+    QLineEdit *port;
+    QPlainTextEdit *payload;
+    QPlainTextEdit *textLog;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(585, 692);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        verticalLayout_2 = new QVBoxLayout(centralwidget);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName("label_4");
+
+        horizontalLayout->addWidget(label_4);
+
+        sendButton = new QPushButton(centralwidget);
+        sendButton->setObjectName("sendButton");
+        sendButton->setMaximumSize(QSize(128, 16777215));
+
+        horizontalLayout->addWidget(sendButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setSizeConstraint(QLayout::SetDefaultConstraint);
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+
+        horizontalLayout_2->addWidget(label);
+
+        ip1 = new QLineEdit(centralwidget);
+        ip1->setObjectName("ip1");
+        ip1->setMaxLength(32767);
+
+        horizontalLayout_2->addWidget(ip1);
+
+        ip2 = new QLineEdit(centralwidget);
+        ip2->setObjectName("ip2");
+        ip2->setMaxLength(3);
+
+        horizontalLayout_2->addWidget(ip2);
+
+        ip3 = new QLineEdit(centralwidget);
+        ip3->setObjectName("ip3");
+        ip3->setMaxLength(3);
+
+        horizontalLayout_2->addWidget(ip3);
+
+        ip4 = new QLineEdit(centralwidget);
+        ip4->setObjectName("ip4");
+        ip4->setMaxLength(3);
+
+        horizontalLayout_2->addWidget(ip4);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName("horizontalLayout_5");
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+
+        horizontalLayout_5->addWidget(label_2);
+
+        port = new QLineEdit(centralwidget);
+        port->setObjectName("port");
+        port->setMaxLength(5);
+
+        horizontalLayout_5->addWidget(port);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout_5);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        payload = new QPlainTextEdit(centralwidget);
+        payload->setObjectName("payload");
+
+        verticalLayout_2->addWidget(payload);
+
+        textLog = new QPlainTextEdit(centralwidget);
+        textLog->setObjectName("textLog");
+
+        verticalLayout_2->addWidget(textLog);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 585, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Command", nullptr));
+        sendButton->setText(QCoreApplication::translate("MainWindow", "SEND", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "IP Address", nullptr));
+        ip1->setInputMask(QString());
+        ip1->setText(QCoreApplication::translate("MainWindow", "127", nullptr));
+        ip1->setPlaceholderText(QString());
+        ip2->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        ip2->setPlaceholderText(QString());
+        ip3->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        ip3->setPlaceholderText(QString());
+        ip4->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
+        ip4->setPlaceholderText(QString());
+        label_2->setText(QCoreApplication::translate("MainWindow", "PORT", nullptr));
+        port->setText(QCoreApplication::translate("MainWindow", "8888", nullptr));
+        port->setPlaceholderText(QString());
+        payload->setPlainText(QCoreApplication::translate("MainWindow", "Hello World", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
